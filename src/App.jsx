2c4b7bfc3e31d1aa7e24fb84967a7613aa7b0cec
Nata <JosephAlvarengaBeech@protonmail.com>
2020-06-25 10:42:07 -0700

    skeleton started

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {BrowserRouter as Router,route} from 'react-router-dom';
+import Navbar from './components/navBar.jsx'
+import './App.css';
+
+
+
+function App() {
+  return (
+    <Router>
+    {/* navbar needs to be added */}
+    <Navbar/>
+    <br/>
+    {/* route for the restaunrant list */}
+    <Route path="/" exact component={StoreList}/>
+    {/* route to render a single restaurant with all its dishes  they will be displayed in the form of cards */}
+    <Route path="/store/:id" component={Store}/>
+    {/* route to add a restaurant */}
+    <Route path="/createStore" component={addStore}/>
+    {/* route to add a dish  */}
+    <Route path="/store/:id/addDish" component={AddDish}/>
+
+    
+    </Router>
+  );
+}
+
+export default App;
+
