7b52f6241fad76fd9ecdeef109fca2b198576126
abdella206 <f.abdella1@yahoo.com>
2020-07-08 18:00:34 -0700

    added the DishList page and fixed the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router,route} from 'react-router-dom';
+//import {BrowserRouter as Router,route} from 'react-router-dom';
 import Navbar from './components/navBar.jsx'
 import './App.css';
 import StoreList from './components/storeList.jsx'
@@ -9,24 +9,26 @@ import AddStoreForm from './components/addStore.jsx'
 
 function App() {
   return (
-    
-    //<AddStoreForm/>
-   <StoreList/>
-    // <Router>
-    // {/* navbar needs to be added */}
-    // <Navbar/>
-    // <br/>
-    // {/* route for the restaunrant list */}
-    // <Route path="/" exact component={StoreList}/>
-    // {/* route to render a single restaurant with all its dishes  they will be displayed in the form of cards */}
-    // <Route path="/store/:id" component={Store}/>
-    // {/* route to add a restaurant */}
-    // <Route path="/createStore" component={addStore}/>
-    // {/* route to add a dish  */}
-    // <Route path="/store/:id/addDish" component={AddDish}/>
+    <>
+    {/* <AddStoreForm/> */}
+    <Navbar/>
+    {/* <StoreList/> */}
+    {/* <Router> */}
+    {/* navbar needs to be added */}
+    {/* <br/> */}
+    {/* route for the restaunrant list */}
+    {/* <Route path="/" exact component={StoreList}/> */}
+    {/* route to render a single restaurant with all its dishes  they will be displayed in the form of cards */}
+    {/* <Route path="/store/:id" component={Store}/> */}
+    {/* route to add a restaurant */}
+    {/* <Route path="/createStore" component={addStore}/> */}
+    {/* route to add a dish  */}
+    {/* <Route path="/store/:id/addDish" component={AddDish}/> */}
 
     
-    // </Router>
+    {/* </Router> */}
+
+    </>
   );
 }
 
---
+++ src/components/DishList.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Card, CardActions, CardContent, CssBaseline, Button, Typography, Container } from '@material-ui/core/'
+
+
+
+
+const DishList = () => {
+    const [data, setData] = useState([])
+    const api = 'http://localhost:3001/'
+    useEffect(() => {
+        const fetchData = async () => {
+            axios.get(api + 'stores/dishes')
+                .then(res => {
+                    console.log(res.data)
+                    console.log(res.data[0].establishment)
+                    setData(res.data)
+                })
+        }
+        fetchData();
+    }, [])
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div>
+                {data.map(dish => {
+                    return (<Card key={dish.id}>
+                        <CardContent>
+                            <hr />
+                            <Typography gutterBottom variant="h5" component="h2">{dish.dish}</Typography>
+
+                            <Typography variant="body2" color="textSecondary" component="p">{dish.price}</Typography>
+                            <br />
+                            <Typography variant="body2" color="textSecondary" component="p">{dish.review}</Typography>
+                            <br />
+                            <Typography variant="body2" color="textSecondary" component="p">{dish.name}</Typography>
+                        </CardContent>
+                    </Card>)
+                })}
+            </div>
+        </Container>
+    )
+    // write a axios.get and then map that data into cards 
+    //after that you need to push the data into graphs 
+}
+
+export default DishList
