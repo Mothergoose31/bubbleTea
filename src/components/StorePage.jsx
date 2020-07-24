4039207f42d093cdc914578473d5d90937572de2
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2020-07-23 17:31:19 -0700

    start of the store page

--- src/components/StorePage.jsx
+++ src/components/StorePage.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
+import axios from 'axios'
 import {Container, Typography} from '@material-ui/core'
 
 
 export default function StorePage ({match}){
     const {params:{storeId}} = match
+    console.log(storeId)
     console.log('hitting the route')
+    const [data,setData]=useState([])
+    const api = 'http://localhost:3001/'
+    useEffect(()=>{
+        const fetchData = async() =>{
+            axios.get(api+`stores/store/${storeId}`)
+                .then(res=>{
+                    console.log(res.data)
+                    console.log(res.data.dishes)
+                    setData(res.data)
+                })
+            }
+            fetchData();
+    },[])
     return(
         <Container>
             <Typography gutterBottom variant="h5" component="h2">Storeid :{storeId}</Typography>
